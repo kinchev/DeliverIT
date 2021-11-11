2c9a8a7f14ef3e1a9fc541f7b3fda4d1141e59e0
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-11-11 23:54:02 +0200

    Implement incomingParcels method.

---
+++ src/main/java/com/telericacademy/web/deliverit/controllers/mvc/ParcelMvcController.java
@@ -0,0 +1,9 @@
+package com.telericacademy.web.deliverit.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/parcels")
+public class ParcelMvcController {
+}
